04657fab72de45ecf84bde18fdb61e206acec6b6
Hamza75950 <hamza.niazi004@gmail.com>
2024-02-29 17:17:28 +0500

    Working on MainDashboard View

---
+++ TimeLoggerView/MainDashboardView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimeLoggerView
+{
+    /// <summary>
+    /// Interaction logic for MainDashboardView.xaml
+    /// </summary>
+    public partial class MainDashboardView : Window
+    {
+        public MainDashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+    
+
