e89a036f1e1125d542deec352f898ed84413bef4
Hamza75950 <hamza.niazi004@gmail.com>
2024-03-04 16:35:59 +0500

    Redesign UI, add a signout button and its functionality

--- TimeLoggerView/MainDashboardView.xaml.cs
+++ TimeLoggerView/MainDashboardView.xaml.cs
@@ -24,10 +24,111 @@ namespace TimeLoggerView
         {
             InitializeComponent();
         }
-
+       
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            this.Close();
+            loginWindow.Show();
+
+        }
+
+        private void btn_Home_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btnHome.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+        }
+
+        private void btn_User_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_User.Background = brush;
+            btnHome.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+
+        }
+
+        private void btn_TimeLogs_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_TimeLogs.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btnHome.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+        }
+
+        private void btn_Projects_Click(object sender, RoutedEventArgs e)
+        {
+
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_Projects.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btnHome.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+        }
+
+        private void btn_Requests_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_Requests.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btnHome.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+        }
+
+        private void btn_Audit_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_Audit.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btnHome.Background = Brushes.Transparent;
+            btn_Reports.Background = Brushes.Transparent;
+        }
+
+        private void btn_Reports_Click(object sender, RoutedEventArgs e)
+        {
+            Color customColor = (Color)ColorConverter.ConvertFromString("#383275");
+            SolidColorBrush brush = new SolidColorBrush(customColor);
+            btn_Reports.Background = brush;
+            btn_User.Background = Brushes.Transparent;
+            btn_TimeLogs.Background = Brushes.Transparent;
+            btn_Projects.Background = Brushes.Transparent;
+            btn_Requests.Background = Brushes.Transparent;
+            btn_Audit.Background = Brushes.Transparent;
+            btnHome.Background = Brushes.Transparent;
         }
     }
 }
